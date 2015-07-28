c07ec6f8464992adfc3ac2271fa6942ae27d9025
Tobias Kremer <tobias.kremer@comsysto.com>
2015-07-28 16:22:01 +0200

    correct errors with the configuration

--- src/main/java/io/github/endron/oauth/authserver/Application.java
+++ src/main/java/io/github/endron/oauth/authserver/Application.java
@@ -2,22 +2,42 @@ package io.github.endron.oauth.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @SpringBootApplication
 @EnableAuthorizationServer
+@SessionAttributes("authorizationRequest")
 public class Application extends AuthorizationServerConfigurerAdapter {
 
-    public static void main(final String... args) {
+    public static void main(String... args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Override
-    public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
                 .withClient("testClient")
-                .secret("secret");
+                .authorizedGrantTypes("authorization_code", "refresh_token").scopes("openid")
+                .authorities("ROLE_OAUTH_CLIENT");
+    }
+
+    @Configuration
+    public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .formLogin()
+                        .permitAll()
+                    .and()
+                    .authorizeRequests()
+                        .anyRequest().authenticated();
+        }
     }
 }
