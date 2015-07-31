16e506718341d196b4fcc21eeae0cf714e6d34b9
Tobias Kremer <tobias.kremer@comsysto.com>
2015-07-31 08:47:22 +0200

    add support for distributed sessions
    add Spring Session to allow for distributed sessions. With this it
    should be possible to have multiple instances of the service running in
    parrallel. The disadvantage is that now a Redis instance is required.

--- src/main/java/io/github/endron/oauth/authserver/Application.java
+++ src/main/java/io/github/endron/oauth/authserver/Application.java
@@ -2,16 +2,21 @@ package io.github.endron.oauth.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.session.web.http.HttpSessionStrategy;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @SpringBootApplication
 @EnableAuthorizationServer
+@EnableRedisHttpSession
 @SessionAttributes("authorizationRequest")
 public class Application extends AuthorizationServerConfigurerAdapter {
 
@@ -41,5 +46,10 @@ public class Application extends AuthorizationServerConfigurerAdapter {
                     .authorizeRequests()
                         .anyRequest().authenticated();
         }
+    }
+
+    @Bean
+    public HttpSessionStrategy httpSessionStrategy() {
+        return new HeaderHttpSessionStrategy();
     }
 }
